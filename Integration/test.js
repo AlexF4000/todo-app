c274c84f348e0c8ccfb8ea958c4911c5faaa4dd4
AlexF4000 <115374268+AlexF4000@users.noreply.github.com>
2024-04-03 23:52:15 -0400

    Finished integration test (#5)

--- Integration/test.js
+++ Integration/test.js
@@ -4,6 +4,8 @@ const { Builder, By, until } = require("selenium-webdriver");
 require("chromedriver");
 
 const INPUT_WRAPPER_LOCATOR = By.css('[data-testid="task-input"]');
+const newTask = "First task";
+const secondTask = "Second task";
 
 describe("TO DO List", () => {
   let driver;
@@ -25,14 +27,12 @@ describe("TO DO List", () => {
   }, 30000);
 
   test("should add a new task via keyboard and verify its presence", async () => {
-    const newTask = 'First task';
-
     const inputWrapper = await driver.wait(
       until.elementLocated(INPUT_WRAPPER_LOCATOR),
       10000
     );
     await driver.wait(until.elementIsVisible(inputWrapper), 10000);
-    const input = await inputWrapper.findElement(By.css('input'));
+    const input = await inputWrapper.findElement(By.css("input"));
     await input.sendKeys(newTask, webdriver.Key.ENTER);
 
     const task = await driver.findElement(
@@ -41,17 +41,14 @@ describe("TO DO List", () => {
     expect(await task.getText()).toContain(newTask);
   }, 30000);
 
-
   test("should add a new task via button and verify its presence", async () => {
-    const newTask = 'Second task';
-
     const inputWrapper = await driver.wait(
       until.elementLocated(INPUT_WRAPPER_LOCATOR),
       10000
     );
     await driver.wait(until.elementIsVisible(inputWrapper), 10000);
-    const input = await inputWrapper.findElement(By.css('input'));
-    await input.sendKeys(newTask);
+    const input = await inputWrapper.findElement(By.css("input"));
+    await input.sendKeys(secondTask);
 
     const addTaskButton = await driver.wait(
       until.elementLocated(By.css('[data-testid="add-test-button"]')),
@@ -64,5 +61,35 @@ describe("TO DO List", () => {
       By.css(`[data-testid="task-${newTask}"]`)
     );
     expect(await task.getText()).toContain(newTask);
+  }, 30000);
+
+  test("should toggle the task via checkbox and verify its presence", async () => {
+    const taskItem = await driver.findElement(
+      By.css(`[data-testid="task-${newTask}"]`)
+    );
+    const taskCheckbox = await taskItem.findElement(
+      By.css("input[type='checkbox']")
+    );
+    await taskCheckbox.click();
+  }, 30000);
+
+  test("should delete the task via button and verify its absence", async () => {
+    const taskItem = await driver.findElement(
+      By.css(`[data-testid="task-${secondTask}"]`)
+    );
+
+    const deleteButton = await taskItem.findElement(
+      By.css(`[data-testid="delete-test-button"]`)
+    );
+    await deleteButton.click();
+
+    await driver.sleep(1000);
+
+    const tasks = await driver.findElements(
+      By.css(`[data-testid="task-${secondTask}"]`)
+    );
+
+    const taskPresent = tasks.length > 0;
+    expect(taskPresent).toBe(false);
   }, 30000);
 });
